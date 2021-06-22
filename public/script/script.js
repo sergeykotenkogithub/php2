8d286a8d01acec5998d60cc3de6b9c131b14b403
sergeykotenko <sergeykotenko@bk.ru>
2021-06-22 06:45:30 +0300

    'nice__async_иуые'

--- public/script/script.js
+++ public/script/script.js
@@ -1,11 +1,8 @@
-// Максимальная высота экрана
-
-
-
-// const windowScrollTop = window.pageYOffset;
+// const windowScrollTop = window.pageYOffset; - высота
 
 window.addEventListener('scroll', () => {
 
+    // Максимальная высота экрана
     let scrollHeight = Math.max(
         document.body.scrollHeight, document.documentElement.scrollHeight,
         document.body.offsetHeight, document.documentElement.offsetHeight,
@@ -20,7 +17,6 @@ window.addEventListener('scroll', () => {
         let txt = count.innerText;
          console.log('Ура');
         (async () => {
-            // const response = await fetch(`/?action=buy&price=${price}&id=${id}`);
             const response = await fetch(`/?c=async&a=catalog&page=two&count=${txt}`);
             const answer = await response.json();
             document.getElementById('count').innerText = `${answer.count}`;
@@ -31,14 +27,6 @@ window.addEventListener('scroll', () => {
 
             $answerCatalog.forEach(function(item) {
                 let addCatalog = document.getElementById('addCatalog')
-                // let div = document.createElement('div');
-                // let div2 = document.createElement('div');
-                // let img = document.createElement('img');
-                // img.className = "goods__img"
-                // // let src = document.createElement('img');
-                // img.src = `/img/goods/${item['image']}`;
-                // div.className = "goods";
-                // addCatalog.insertAdjacentHTML('beforebegin', `<a href=/?c=product&a=card&id${item['id']}>`);
                 addCatalog.insertAdjacentHTML('beforeend',
                 `                    
                     <div id="addAsyncCatalog" class="goods">                    
@@ -52,25 +40,6 @@ window.addEventListener('scroll', () => {
                         </div>                        
                     </div>                        
                 `);
-
-                // <div id="addAsyncCatalog" className="goods">
-                //
-                //     <a href="/?c=product&a=card&id=<?=$item->id?>">
-                //         <img className="goods__img" src="/img/goods/<?=$item->image?>" alt="">
-                //     </a>
-                //
-                //     <div className="goods__buy">
-                //         <p className="goods__name"> <?=$item->name?> </p>
-                //         <p className="goods__price rub">Цена: <?=$item->price?></p>
-                //         <a className="goods__iconBuy" href="#"><img src="/img/icon/cart.svg" alt=""></a>
-                //     </div>
-                //
-                // </div>
-
-                // div.innerHTML = item['name'];
-                // div2.insertAdjacentHTML()
-                // d.insertAdjacentHTML('afterend', `<div>${item['name']}</div>`);
-                // addCatalog.appendChild()
             })
         })();
     }
