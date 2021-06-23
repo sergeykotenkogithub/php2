c9af409b206601933310c7051e129b63e36cf292
sergeykotenko <sergeykotenko@bk.ru>
2021-06-23 06:58:23 +0300

    'all_lesson4'

--- controllers/AsyncController.php
+++ controllers/AsyncController.php
@@ -11,13 +11,12 @@ class AsyncController extends Controller
     public function actionCatalog() {
         if($_GET['page'] == 'two') {
             $count = $_GET['count'];
-            $count += 2;         // 2 4 6 8
-//            $after = $count + 2; // 4 6 8 10
-            $catalog = Product::getLimitAjax($count, $count);
+            $count += 2;
+            $catalog = Product::getLimitAjax($count, 2);  // первая смещение,  вторая цифра это количество записей которое показывается.
             echo json_encode([
                'count' => $count,
                'catalog' => $catalog
-            ]);
+            ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
             die();
         }
     }
@@ -27,7 +26,7 @@ class AsyncController extends Controller
             $id = $_GET['id'];
             $price = $_GET['price'];
         }
-//        Product:
-        echo json_encode(['count' => $id]);
+        //  Product:
+        echo json_encode(['count' => $id], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     }
 }
