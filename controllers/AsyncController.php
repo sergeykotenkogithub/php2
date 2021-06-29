477fd0c50dc54423ff95b8404d78d2e9458bda7e
sergeykotenko <sergeykotenko@bk.ru>
2021-06-29 04:12:49 +0300

    'start_request'

--- controllers/AsyncController.php
+++ controllers/AsyncController.php
@@ -11,12 +11,14 @@ use app\models\Product;
 class AsyncController extends Controller
 
 {
-    // В Каталоге: Скролинг
+    //.............................В Каталоге: Скролинг......................................................
+
     public function actionCatalog() {
-        if($_GET['page'] == 'two') {
-            $count = $_GET['count'];
-            $count += PRODUCT_PER_PAGE;
-            $catalog = Product::getLimitAjax($count, PRODUCT_PER_PAGE);  // первая смещение,  вторая цифра это количество записей которое показывается.
+
+        if((new Request())->getParams()['page'] == 'two') {
+            $count = (new Request())->getParams()['count'];
+            $count += PRODUCT_PER_PAGE; // Параметр в config
+            $catalog = Product::getLimitAjax($count, PRODUCT_PER_PAGE);  // первая - смещение,  вторая цифра - это количество записей которое показывается.
             echo json_encode([
                'count' => $count,
                'catalog' => $catalog
@@ -25,22 +27,22 @@ class AsyncController extends Controller
         }
     }
 
-    // В Каталоге: Добавление товара в корзину при нажатие кнопки купить
+    //...............В Каталоге: Добавление товара в корзину при нажатие кнопки купить......................
+
     public function actionBuy() {
-        $id = $_GET['id'];
-        $price = $_GET['price'];
+
+        $id = (new Request())->getParams()['id'];
+        $price = (new Request())->getParams()['price'];
         $session = session_id();
         $count = Basket::countSum('quantity', 'session_id', $session);
         (new Basket($id, "$session", $price, $price, 1))->save();
         echo json_encode(['count' => $count + 1], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     }
 
-    // В Корзине: при нажатие кнопки удалить - удаляет полностью
+    //.......В Корзине: при нажатие кнопки удалить - удаляет полностью.....................................
+
     public function actionDelete() {
         $session = session_id();
-
-        // Через GET
-        // $id = $_GET['id'];
 
         // Через POST
 
@@ -53,6 +55,9 @@ class AsyncController extends Controller
         ];
 
         echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+        // Через GET
+        // $id = $_GET['id'];
 
         // Второй вариант через глобальный:
 
