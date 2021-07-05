a8ba0692b2863250c47b2c266a5e2e55caccccda
sergeykotenko <sergeykotenko@bk.ru>
2021-07-05 11:49:56 +0300

    'add_app_async'

--- controllers/AsyncController.php
+++ controllers/AsyncController.php
@@ -5,11 +5,8 @@
 namespace app\controllers;
 
 use app\engine\App;
-use app\engine\Request;
-use app\engine\Session;
 use app\models\entities\Basket;
-use app\models\repositories\BasketRepository;
-use app\models\repositories\OrderRepository;
+
 
 class AsyncController extends Controller
 
@@ -22,29 +19,36 @@ class AsyncController extends Controller
 
         if ($page == 'two') {
             $count = App::call()->request->getParams()['count'];
-            $count += App::call()->config['product_per_page'] ; // Параметр в config
-            $catalog = (new OrderRepository())->getLimitAjaxDesc($count, App::call()->config['product_per_page']);
+            $count += App::call()->config['product_per_page'] ; // product_per_page - параметр в config
+            $catalog = App::call()->orderRepository->getLimitAjaxDesc($count, App::call()->config['product_per_page']);
             // первая $count - смещение,  вторая цифра App::call()->config['product_per_page'] - это количество записей которое показывается.
+
             echo json_encode([
                'count' => $count,
                'catalog' => $catalog
             ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
             die();
         }
+
     }
 
     //...............В Каталоге: Добавление товара в корзину при нажатие кнопки купить......................
 
     public function actionBuy() {
 
-        $id = (new Request())->getParams()['id'];
-        $price = (new Request())->getParams()['price'];
-        $session = (new Session())->getId();
-        $basket = new Basket($id, "$session", $price, $price, 1);
-        $count = (new BasketRepository())->countSum('quantity', 'session_id', $session);
+        //...........GET и POST................................
 
-//        (new BasketRepository())->save($basket); //!!! НЕ РАБОТАЕТ ПОЧЕМУ???
-        (new BasketRepository())->insert($basket);
+        $id = App::call()->request->getParams()['id'];
+        $price = App::call()->request->getParams()['price'];
+
+        //....................................................
+
+        $session = App::call()->session->getId();
+        $basket = new Basket($id, "$session", $price, $price, 1);
+        $count = App::call()->basketRepository->countSum('quantity', 'session_id', $session);
+
+//          App::call()->basketRepository->save($basket); //!!! НЕ РАБОТАЕТ ПОЧЕМУ???
+        App::call()->basketRepository->insert($basket);
 
         echo json_encode(['count' => $count + 1], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     }
@@ -52,15 +56,22 @@ class AsyncController extends Controller
     //.......В Корзине: при нажатие кнопки удалить - удаляет полностью.....................................
 
     public function actionDelete() {
+
         //................Через POST.............................
-        $session_id = (new Session())->getId();
-        $id = (new Request())->getParams()['id'];
-        $basket = (new BasketRepository())->getOneAndWhere('id', $id, 'session_id', $session_id);
-        (new BasketRepository())->delete($basket);
+
+        $session_id = App::call()->session->getId();
+        $id = App::call()->request->getParams()['id'];
+        $basket = App::call()->basketRepository->getOneAndWhere('id', $id, 'session_id', $session_id);
+
+        App::call()->basketRepository->delete($basket);
+
         $response = [
-            'count' =>  (new BasketRepository())->countSum('quantity', 'session_id', $session_id),
-            'sum' => (new BasketRepository())->countSum('price', 'session_id', $session_id)
+            'count' =>  App::call()->basketRepository->countSum('quantity', 'session_id', $session_id),
+            'sum' => App::call()->basketRepository->countSum('price', 'session_id', $session_id)
         ];
+
         echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
     }
+
 }
