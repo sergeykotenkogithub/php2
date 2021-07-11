d8c462be2fd8d3b3866b7a2df2aae415f1e27922
sergeykotenko <sergeykotenko@bk.ru>
2021-07-11 04:05:51 +0300

    'feedback_add'

--- controllers/FeedbackController.php
+++ controllers/FeedbackController.php
@@ -2,9 +2,48 @@
 
 namespace app\controllers;
 
+use app\engine\App;
+
 final class FeedbackController extends Controller
 {
+
     public function actionAll() {
-        echo $this->render('feedback');
+
+       $feedback_product =  App::call()->feedbackRepository->getAllFeedbackProduct();
+       $feedback_site = App::call()->feedbackRepository->getAllFeedbackSite();
+
+       // Поле формы добавить отзыв
+
+       $feedback_answer =  App::call()->request->getParams()['feedback_answer'];
+       $name =  App::call()->request->getParams()['name'];
+       $textarea =  App::call()->request->getParams()['textarea'];
+
+
+
+        // Если выбран сайт или товар
+
+        if ($feedback_answer == 'site') {
+            App::call()->feedbackRepository->feedback_site($name, $textarea);
+            App::call()->session->set('message', 'Ваш отзыв добавлен');
+            $message = App::call()->session->get('message');
+            $unset = App::call()->session->get('message');
+            unset($unset);
+        }
+
+        if  ($feedback_answer != 'site' & isset($name)) {
+            App::call()->feedbackRepository->feedback_goods($name, $textarea, $feedback_answer);
+            App::call()->session->set('message', 'Ваш отзыв добавлен');
+            $message = App::call()->session->get('message');
+            $unset = App::call()->session->get('message');
+            unset($unset);
+        }
+
+
+
+        echo $this->render('feedback', [
+            'feedback_product' => $feedback_product,
+            'feedback_site' => $feedback_site,
+            'message' => $message
+        ]);
     }
 }
